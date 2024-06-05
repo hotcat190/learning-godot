dd57470172d8ad461e0c3de161472002bd155a47
hotcat190 <nguyendinhnguyen151204@gmail.com>
2024-06-06 00:28:11 +0700

    update collision and refactor animation

---
+++ Dash.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+func _on_player_dash():
+	#Play animations
+	$AnimationPlayer.play("new_animation")
+	$explosion.play("explosion_fade_out")
+	$explodinghead.emitting = true
+	
+	#Play sound
+	$AudioStreamPlayer.playing = true
+	
+	#Start cooldown timer
+	$DashCooldownTimer.start(-1)
+	
--- Player.gd
+++ Player.gd
@@ -1,20 +1,20 @@
-extends Area2D
+extends CharacterBody2D
 
-const angular_speed = PI
+signal dash
 
-@export var speed = 400
+const ANGULAR_SPEED = PI
+const DASH_MULTIPLIER: int = 25
+
+@export var speed = 6
 
 @onready 
-var dash_cooldown:Timer = get_node("DashCooldownTimer")
+var dash_cooldown:Timer = get_node("Dash/DashCooldownTimer")
 
 func _ready():
 	dash_cooldown.timeout.connect(_on_timer_timeout)	
 	
 func _on_timer_timeout():
 	dash_cooldown.stop()
-	
-func _on_button_pressed():
-	set_process(not is_processing())
 
 func _process(delta):
 	var direction = 0
@@ -23,26 +23,24 @@ func _process(delta):
 	if Input.is_action_pressed("move_right"):
 		direction = 1
 
-	rotation += angular_speed * direction * delta	
+	rotation += ANGULAR_SPEED * direction * delta
 	
-	var velocity = Vector2.ZERO
+	var distance = Vector2.ZERO
 	if Input.is_action_pressed("move_up"):
-		velocity = Vector2.UP.rotated(rotation) * speed
+		distance = Vector2.UP.rotated(rotation) * speed
 	
 	if Input.is_action_pressed("move_down"):
-		velocity = - Vector2.UP.rotated(rotation) * speed
+		distance = - Vector2.UP.rotated(rotation) * speed
 		
 	if Input.is_action_pressed("dash"):
 		if (dash_cooldown.is_stopped()):
-			$explodinghead.emitting = true
-			$AudioStreamPlayer.playing = true
-			$AnimationPlayer.play("new_animation")
-			$explosion.play("explosion_fade_out")
-			velocity = Vector2.UP.rotated(rotation) * speed * 40
+			distance = Vector2.UP.rotated(rotation) * speed * DASH_MULTIPLIER
 			if Input.is_action_pressed("move_down"):
-				velocity = - velocity
-			dash_cooldown.start(-1)
-	position += velocity * delta
+				distance = - distance			
+			dash.emit()
+		
+	move_and_collide(distance)
+
 
 func _on_pause_button_toggled(toggled_on):
 	set_process(not is_processing())
